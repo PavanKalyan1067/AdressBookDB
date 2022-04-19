48b886bd9947291bfb1f7f5071e2df246ffdd77d
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-19 10:42:15 +0530

    Creating a table

--- AdressBookDB.sql
+++ AdressBookDB.sql
@@ -1,1 +1,12 @@
 create database Address_Book_ServiceDB;
+------ UC 2: Create Table ------
+create table Address_Book_Table
+(FirstName varchar(100),
+SecondName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+zip BigInt,
+PhoneNumber BigInt,
+Email varchar(200)
+)
