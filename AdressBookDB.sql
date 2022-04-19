7456f57cb0248aa8604fa714b8790d2241bc8e58
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-19 10:53:38 +0530

    Retirving count of persons based on city name

--- AdressBookDB.sql
+++ AdressBookDB.sql
@@ -41,3 +41,10 @@ where FirstName='Rujula' and SecondName='Mathialagan'
 select * 
 from Address_Book_Table
 where City='Bangalore' or State='Karnataka'
+
+------ UC 7: Ability to Retrieve Count of Person belonging to a City or State ------
+Insert into Address_Book_Table(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email) 
+values('meena','Vargheese','22 Catherine Street','Hosur','Karnataka',2545001,957575050,'Meena@gmail.com')
+select Count(*),state,City
+from Address_Book_Table
+Group by state,City
